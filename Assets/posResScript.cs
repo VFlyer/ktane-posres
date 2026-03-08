0887b837a7f8f213c384645f53945fd3bd49cec4
rand-06 <pogtamaxim10@gmail.com>
2026-03-08 09:50:24 +0300

    upload the changed code

--- Assets/posResScript.cs
+++ Assets/posResScript.cs
@@ -2,32 +2,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
+using System.Text.RegularExpressions;
+using KeepCoding;
 using UnityEngine;
-using KModkit;
 
 public class BaseNInt
 {
     private static readonly string baseN = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_-+=|\\{}:;\"'<>,.?/~[]`";
-    private static int baseSize = baseN.Length;
     private string v;
+    private static int numberBase, amountOfDigits;
     BaseNInt(string value) {v =  value;}
+
+    public static int NumberBase {
+        get { return  numberBase; }
+        set { numberBase = value; } }
+
+    public static int AmountOfDigits
+    {
+        get { return  amountOfDigits; }
+        set { amountOfDigits = value; }
+    }
 
     public static BaseNInt Random()
     {
-        return new BaseNInt(String.Join("", Enumerable.Range(0, 20).Select(x => baseN[UnityEngine.Random.Range(0, baseSize)].ToString()).ToArray()));
+        return new BaseNInt(String.Join("", Enumerable.Range(0, amountOfDigits).Select(x => baseN[UnityEngine.Random.Range(0, numberBase)].ToString()).ToArray()));
     }
 
     public static BaseNInt operator +(BaseNInt a, BaseNInt b)
     {
         string ans = "";
         bool carry = false;
-        for (int i = 19; i > -1; i--)
+        for (int i = amountOfDigits - 1; i > -1; i--)
         {
             int digitSum = baseN.IndexOf(a.v[i]) + baseN.IndexOf(b.v[i]) + (carry?1:0);
-            carry = digitSum > baseSize-1;
-            ans = baseN[digitSum % baseSize] + ans;
+            carry = digitSum > numberBase-1;
+            ans = baseN[digitSum % numberBase] + ans;
         }
         return new BaseNInt(ans);
     }
@@ -44,7 +53,7 @@ public class BaseNInt
 
     public string toBase36()
     {
-        List<int> originalList = Enumerable.Range(0, 20).Select(x => baseN.IndexOf(v[x])).ToList();
+        List<int> originalList = Enumerable.Range(0, amountOfDigits).Select(x => baseN.IndexOf(v[x])).ToList();
         List<int> answerList = new List<int>();
         while (originalList.Count > 0)
         {
@@ -53,7 +62,7 @@ public class BaseNInt
 
             foreach (int d in originalList)
             {
-                int temp = carry * baseSize + d;
+                int temp = carry * numberBase + d;
                 int q = temp / 36;
                 carry = temp % 36;
                 quotient.Add(q);
@@ -110,6 +119,10 @@ public class posResScript : MonoBehaviour
     private string inputtedAnswer = "";
     private bool stopCorouts;
 
+    private bool tpReady;
+    
+    private int numberBase, amountOfDigits, timer;
+
     private int max(int a, int b){ return a > b ? a : b; }
     
     void refreshStage1()
@@ -117,8 +130,16 @@ public class posResScript : MonoBehaviour
         Audio.HandlePlaySoundAtTransform(clips[0].name, transform);
         A += X;
         string ans = "";
-        ans += BaseNInt.Random().ToString().Substring(0,15) + String.Join("",Enumerable.Range(0, 5).Select(x => " ").ToArray()) + "\n";
-        ans += BaseNInt.Random().ToString().Substring(0,16) + String.Join("",Enumerable.Range(0, 4).Select(x => " ").ToArray()) + "\n";
+        int diff = BaseNInt.AmountOfDigits - 20;
+        if (diff > -9)
+        {
+            ans += BaseNInt.Random().ToString().Substring(0, 15+(diff-1)/2) +
+                   String.Join("", Enumerable.Range(0, (diff+11)/2).Select(x => " ").ToArray()) + "\n";
+            ans += BaseNInt.Random().ToString().Substring(0, 16+(diff-1)/2) +
+                   String.Join("", Enumerable.Range(0, (diff+9)/2).Select(x => " ").ToArray()) + "\n";
+        }
+        else for (int i=0; i<2; i++) ans+=BaseNInt.Random()+"\n";
+
         for (int i=0; i<2; i++) ans+=BaseNInt.Random()+"\n";
         ans += A.ToString();
         for (int i=0; i<4; i++) ans+="\n"+BaseNInt.Random();
@@ -128,7 +149,8 @@ public class posResScript : MonoBehaviour
     IEnumerator state1()
     {
         statusBusy = false;
-        int countDown = 1800;
+        int countDown = timer;
+        tpReady = true;
         while (countDown >= 0 && !stopCorouts)
         {
             if (countDown % 20 == 0) refreshStage1();
@@ -190,8 +212,8 @@ public class posResScript : MonoBehaviour
                 if (second) amount2 = 0;
                 else amount1 = 0;
                 Center.color= Color.white;
-                int timer = 0;
-                while (timer < 100)
+                int timerTC = 0;
+                while (timerTC < 100)
                 {
                     if (state != 2) yield break;
                     if (tapQueued)
@@ -199,10 +221,10 @@ public class posResScript : MonoBehaviour
                         Audio.HandlePlaySoundAtTransform(clips[2].name, transform);
                         tapQueued = false;
                         if (second) amount2++; else amount1++;
-                        timer = 0;
+                        timerTC = 0;
                     }
-                    timer++;
-                    ProgressBar.transform.localScale = new Vector3(12*timer/100f, 1, 1);
+                    timerTC++;
+                    ProgressBar.transform.localScale = new Vector3(12*timerTC/100f, 1, 1);
                     yield return new WaitForSeconds(0.01f);
                 }
                 ProgressBar.transform.localScale = new Vector3(0, 1, 1);
@@ -212,7 +234,7 @@ public class posResScript : MonoBehaviour
                     if (second)
                     {
                         second = false;
-                        print("amount1: " + amount1 +  ", amount2: " + amount2);
+                        Debug.LogFormat("[Positive-Resistance #{0}] Inputted ({1},{2}), which is {3}.", ModuleId, amount1, amount2, tapCodeString[6 * (amount1 - 1) + amount2 - 1]);
                         inputtedAnswer += tapCodeString[6 * (amount1 - 1) + amount2 - 1];
                         Center.text = inputtedAnswer.Substring(max(0,inputtedAnswer.Length - 7));
                     }
@@ -234,7 +256,7 @@ public class posResScript : MonoBehaviour
                 ProgressBar.transform.localScale = new Vector3(0, 1, 1);
                 if (!tapQueued)
                 {
-                    inputtedAnswer = inputtedAnswer.Substring(0, inputtedAnswer.Length - 1);
+                    inputtedAnswer = inputtedAnswer.Substring(0, max(0,inputtedAnswer.Length - 1));
                     Center.text = inputtedAnswer.Substring(max(0,inputtedAnswer.Length - 7));
                 }
             }
@@ -271,7 +293,8 @@ public class posResScript : MonoBehaviour
         coverBusy = true;
         statusBusy = true;
         Center.text = "...!";
-        print("inputtedAnswer = " + inputtedAnswer + ", expected: " + X.toBase36() + "; result = "+ (inputtedAnswer == X.toBase36()));
+        Debug.LogFormat("[Positive-Resistance #{0}] Submitted {1} and expected {2} . {3}.", ModuleId, inputtedAnswer, X.toBase36(), 
+            X.toBase36()==inputtedAnswer?"Correct":"Incorrect");
         yield return new WaitForSeconds(2f);
         Audio.HandlePlaySoundAtTransform(clips[4].name, transform);
         for (int i = 0; i < 41; i++)
@@ -296,8 +319,8 @@ public class posResScript : MonoBehaviour
             A = BaseNInt.Random();
             coverBusy = false;
             statusBusy = false;
-            print(X);
-            print(X.toBase36());
+            tpReady = false;
+            Debug.LogFormat("[Positive-Resistance #{0}] Your new answer is {1} ({2} in base-36).", ModuleId, X, X.toBase36());
             state = 0;
             presses = 5;
             Center.text = "5";
@@ -343,6 +366,18 @@ public class posResScript : MonoBehaviour
 
     void Awake()
     {
+        
+        ModConfig<PosresSettings> modConfig = new ModConfig<PosresSettings>("PosresSettings");
+        Settings = modConfig.Settings;
+        modConfig.Settings = Settings;
+        TryOverrideMission();
+        numberBase = Settings.numberBase < 2 || Settings.numberBase > 94?94:Settings.numberBase;
+        amountOfDigits = Settings.amountOfDigits < 1 || Settings.amountOfDigits > 20?20:Settings.amountOfDigits;
+        timer = Settings.timer < 10?1800:Settings.timer;
+        
+        BaseNInt.NumberBase = numberBase;
+        BaseNInt.AmountOfDigits = amountOfDigits;
+        
         Mess.text = "";
         Center.text = "5";
         ProgressBar.transform.localScale = new Vector3(0, 1, 1);
@@ -355,10 +390,138 @@ public class posResScript : MonoBehaviour
         X = BaseNInt.Random();
         A = BaseNInt.Random();
         
-        print(X);
-        print(X.toBase36());
+        Debug.LogFormat("[Positive-Resistance #{0}] Base-{1}, {2} digits.", ModuleId, BaseNInt.NumberBase, BaseNInt.AmountOfDigits);
+        Debug.LogFormat("[Positive-Resistance #{0}] Your answer is {1} ({2} in base-36).", ModuleId, X, X.toBase36());
         
         Cover.OnInteract += delegate { if (coverBusy) return false; modulePress(); return false;};
         Status.OnInteract += delegate{if (statusBusy) return false; statusPress(); return false;};
+    }
+    
+    
+    private PosresSettings Settings = new PosresSettings();
+	
+    void TryOverrideMission()
+    {
+        var desc = Game.Mission.Description ?? "";
+        Match regexMatchCountVariants = Regex.Match(desc, @"\[Positive-Resistance\]\s(\d+),(\d+),(\d+)");
+        if (!regexMatchCountVariants.Success) return;
+        int?[] valueMatches = Enumerable.Range(1,3).Select(x => regexMatchCountVariants.Groups[x].Value.TryParseInt()).ToArray();
+        if (valueMatches[0] != null) Settings.numberBase = valueMatches[0].Value;
+        if (valueMatches[1] != null) Settings.amountOfDigits = valueMatches[1].Value;
+        if (valueMatches[2] != null) Settings.timer = valueMatches[2].Value;
+    }
+	
+    class PosresSettings
+    {
+        public int numberBase = 94;
+        public int amountOfDigits = 20;
+        public int timer = 1800;
+    }
+
+    static Dictionary<string, object>[] TweaksEditorSettings = new Dictionary<string, object>[]
+    {
+        new Dictionary<string, object>
+        {
+            { "Filename", "PosresSettings.json" },
+            { "Name", "Positive-Resistance Settings" },
+            { "Listings", new List<Dictionary<string, object>>{
+                new Dictionary<string, object>
+                {
+                    { "Key", "numberBase" },
+                    { "Text", "Base" },
+                    { "Description", "Base of the numbers. Default and maximum is 94." }
+                },
+                new Dictionary<string, object>
+                {
+                    { "Key", "amountOfDigits" },
+                    { "Text", "Size" },
+                    { "Description", "Amount of digits in the base-N number. Default and maximum is 20." }
+                },
+                new Dictionary<string, object>
+                {
+                    { "Key", "timer" },
+                    { "Text", "Timer" },
+                    { "Description", "Time restriction of Read mode in deciseconds. Default is 1800." }
+                },
+            } }
+        }
+    };
+    
+
+#pragma warning disable 414
+    private readonly string TwitchHelpMessage = "Use !{0} 1-6 to press module 1-6 times. Use !{0} S to press status light. You can use multiple commands at once, for example: !{0} 12 22 31S.";
+#pragma warning restore 414
+    
+    public IEnumerator ProcessTwitchCommand (string Command) {
+        yield return null;
+        string comm = Command.ToUpper();
+        foreach(char c in comm) {if (!"123456 S".Contains(c))
+        {
+            yield return "sendtochaterror Invalid command.";
+            yield break;
+        }}
+        foreach (char c in comm)
+        {
+            switch (c)
+            {
+                case 'S':
+                {
+                    Status.OnInteract();
+                    break;
+                }
+                case ' ':
+                {
+                    break;
+                }
+                case '1': case '2': case '3': case '4': case '5': case '6':
+                {
+                    for (int i = 0; i < c - '0'; i++)
+                    {
+                        Cover.OnInteract();
+                        yield return new WaitForSeconds(.1f);
+                    }
+                    break;
+                }
+            }
+            yield return new WaitForSeconds(2.5f);
+        }
+    }
+
+    public IEnumerator TwitchHandleForcedSolve() {
+        yield return null;
+        if (state == 0)
+        {
+            for (int i=0; i<6; i++) Cover.OnInteract();
+            Status.OnInteract();
+            while (state == 0) yield return new WaitForSeconds(.1f);
+        }
+
+        if (state == 1)
+        {
+            while(!tpReady) yield return new WaitForSeconds(.1f);
+            Status.OnInteract();
+            yield return new WaitForSeconds(1f);
+        }
+
+        if (state == 2)
+        {
+            inputtedAnswer = "";
+            foreach (char c in X.toBase36())
+            {
+                int amount1 = tapCodeString.IndexOf(c)/6+1, amount2 =  tapCodeString.IndexOf(c)%6+1;
+                for (int i = 0; i < amount1; i++)
+                {
+                    Cover.OnInteract();
+                    yield return new WaitForSeconds(.1f);
+                }
+                yield return new WaitForSeconds(2.5f);
+                for (int i = 0; i < amount2; i++) {
+                    Cover.OnInteract();
+                    yield return new WaitForSeconds(.1f);
+                }
+                yield return new WaitForSeconds(2.5f);
+            }
+            Status.OnInteract();
+        }
     }
 }
